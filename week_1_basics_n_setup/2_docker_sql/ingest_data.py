e3523b7e87a954b1453005df05e91d65ab639172
axxadvighess <kosymcqueen@gmail.com>
2023-02-08 01:01:24 +0100

    GCP and Terraform environment setup

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -4,65 +4,60 @@
 import os
 import argparse
 
-
-import pandas as pd
-
 from time import time
 
+import pandas as pd
 from sqlalchemy import create_engine
 
 
 def main(params):
     user = params.user
     password = params.password
-    host = params.host
-    port = params.port
+    host = params.host 
+    port = params.port 
     db = params.db
     table_name = params.table_name
     url = params.url
-
-
-
     csv_name = 'output.csv'
 
-    # download the csv
+    # download the CSV file
     os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-
-    engine.connect()
-
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000) 
-
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-
-
-    get_ipython().run_line_magic('time', "df.to_sql(name=table_name, con=engine, if_exists='append')")
-
+    df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
     while True:
-        t_start = time()
-        
-        df = next(df_iter)
-        
-        df.to_sql(name=table_name, con=engine, if_exists='append')
-        
-        t_end = time()
-        
-        print('inserted another chunk..., took %.3f second' % (t_end - t_start))
+        try:
+            t_start = time()
+
+            df = next(df_iter)
+
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+        except StopIteration:
+            print('completed')
+            break
 
 
 if __name__ == '__main__':
+    # Parse the command line arguments and calls the main program
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
     parser.add_argument('--user', help='user name for postgres')
@@ -73,12 +68,6 @@ if __name__ == '__main__':
     parser.add_argument('--table_name', help='name of the table where we will write the results to')
     parser.add_argument('--url', help='url of the csv file')
 
-
     args = parser.parse_args()
 
-
     main(args)
-
-
-
-
