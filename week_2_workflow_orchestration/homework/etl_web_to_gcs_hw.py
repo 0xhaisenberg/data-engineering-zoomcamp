a2944a122ef7b27955e524e9481cbe6d2f90257f
Kosiso Aniebue <Kosymcqueen@gmail.com>
2023-03-28 12:15:55 +0100

    Add files via upload

---
+++ week_2_workflow_orchestration/homework/etl_web_to_gcs_hw.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+@task(log_prints=True, retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    '''Read taxi data from web to pandas DataFreame'''
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+@task(log_prints=True)
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    data_dir = f'data/{color}'
+    Path(data_dir).mkdir(parents=True, exist_ok=True)
+    path = Path(f'{data_dir}/{dataset_file}.parquet')
+    df.to_parquet(path, compression='gzip')
+    return path
+
+
+@task(log_prints=True)
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcp_cloud_storage_bucket_block = GcsBucket.load("data-zoomcamp-gcs")
+    gcp_cloud_storage_bucket_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+
+@flow()
+def el_web_to_gcs() -> None:
+    ''''This is the main Extract & Load function'''
+
+    color = "green"
+    year = 2020
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz'
+
+    df = fetch(dataset_url)
+    path = write_local(df, color, dataset_file)
+    write_gcs(path)
+
+if __name__ == '__main__':
+    el_web_to_gcs()
